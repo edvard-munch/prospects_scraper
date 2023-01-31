836734e62db748ba87f0e67253a3bfb8896aaf70
edvard_munch <arkchib@gmail.com>
2023-01-31 10:37:38 +0300

    Add hfboards scraping of prospects

--- eliteprospects_scraper.py
+++ eliteprospects_scraper.py
@@ -13,7 +13,7 @@ SEARCH_URL = 'https://www.eliteprospects.com/search/player?q='
 PROSPECTS_MEGATHREAD = 'https://forums.hfboards.com/threads/prospect-megathread.2764521/'
 PROSPECTS_FILE = 'prospects.txt'
 LINKS_FILE = 'links.txt'
-LINK_FORMAT = '[url={}]{}[/url]'
+LINK_FORMAT = '{} {} {}'
 
 PLATFORMS = {
     'Windows': 'win32',
@@ -44,26 +44,22 @@ def scrape_from_page(soup, element_type, attr_type, attr_name, string=None):
 
 def start_scraping():
     links = []
+    hfboards_response = requests.get(PROSPECTS_MEGATHREAD)
 
     with open(PROSPECTS_FILE) as f:
         for index, line in enumerate(f.readlines()):
+
             if not line.strip():
                 continue
 
             print(READ_MESSAGE.format(line.strip()))
             var = line.split()
-            query = f'{var[1]}+{var[2]}'
-            response = requests.get(SEARCH_URL + query, headers=headers)
-            soup = bs4.BeautifulSoup(response.content, PARSER)
+            hf_link = get_hf_link(var, hfboards_response)
+            ep_link = get_ep_link(var)
 
-            try:
-                name_cell = scrape_from_page(soup, 'td', 'class', NAME_CLASS)[0]
-            except IndexError:
-                print('!!!!!!!!!! LINK NOT FOUND !!!!!!!!!')
-                continue
-
-            url = name_cell.find('a').attrs['href']
-            links.append(LINK_FORMAT.format(url, line.strip()))
+            link = LINK_FORMAT.format(line.strip(), ep_link, hf_link)
+            links.append(LINK_FORMAT.format(line.strip(), format_link(ep_link, 'EP'),
+                                            format_link(hf_link, 'HF')))
 
     with open(LINKS_FILE, 'w') as f:
         for index, link in enumerate(links):
@@ -73,7 +69,45 @@ def start_scraping():
     open_file(LINKS_FILE)
 
 
+def format_link(link, text):
+    if link:
+        return f'[url={link}]{text}[/url]'
+    else:
+        return text
+
+
+def get_hf_link(var, megathread):
+    hfboards_soup = bs4.BeautifulSoup(megathread.content, PARSER)
+
+    full_name = f'{var[1]} {var[2]}'
+
     res = scrape_from_page(hfboards_soup, 'a', 'class', 'link link--external',
                            string=regex.compile(fr'(?b)(?:{full_name}){{e<=2}}'))
+
+    if res:
+        hf_url = res[0].attrs['href']
+    else:
+        hf_url = None
+        print(f"HF BOARDS LINK for {full_name} NOT FOUND. Check that player's name is without typos !!!")
+
+    return hf_url
+
+
+def get_ep_link(var):
+    query = f'{var[1]}+{var[2]}'
+    response = requests.get(SEARCH_URL + query)
+    soup = bs4.BeautifulSoup(response.content, PARSER)
+
+    try:
+        name_cell = scrape_from_page(soup, 'td', 'class', NAME_CLASS)[0]
+    except IndexError:
+        print('!!!!!!!!!! LINK NOT FOUND !!!!!!!!!')
+        return
+
+    url = name_cell.find('a').attrs['href']
+
+    return url
+
+
 if __name__ == '__main__':
     start_scraping()
